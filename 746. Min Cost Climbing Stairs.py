808ce3355a6a944c0e9818dd484370ae5b78dc77
Jasmin Babariya <142567817+Jasminbabariya48@users.noreply.github.com>
2025-08-02 16:54:57 +0530

    Add files via upload

---
+++ 746. Min Cost Climbing Stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: list[int]) -> int:
+        # DP IN Tabulation
+        n = len(cost)
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        return dp[n]
+    
+if __name__ == "__main__":
+    solution = Solution()
+    cost = [1,100,1,1,1,100,1,1,100,1]
+    print(solution.minCostClimbingStairs(cost))
