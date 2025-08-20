49b94c4700e6789545a86080074c7db6b6484fa6
Jasmin Babariya <142567817+Jasminbabariya48@users.noreply.github.com>
2025-08-20 22:49:24 +0530

    Add files via upload

---
+++ 392. Is Subsequence.py
@@ -0,0 +1,18 @@
+class solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # DP IN Tabulation
+        dp = [[0] * (len(t) + 1) for _ in range(len(s) + 1)]
+        
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(len(t) - 1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+        return dp[0][0] == len(s)
+    
+if __name__ == "__main__":
+    solution = solution()
+    s = "abc"
+    t = "ahbgdc"
+    print(solution.isSubsequence(s, t))  # Output: True
